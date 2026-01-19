f82a40e558dcf2a1f77b272eb5efb89447893f3d
Md. Akram Hossain <akramshuvro@gmail.com>
2026-01-19 13:21:31 +0600

    update

--- config.sh
+++ config.sh
@@ -1,8 +1,10 @@
-# Create Directories & Copy Local Configs
+#!/bin/bash
+
+# Create Directories
 echo "--- Setting up Configs ---"
 mkdir -p ~/.config/{xdg-desktop-portal,fish,niri,waybar,alacritty,swaync}
 
-# Copying files from the local directory where install.sh is located
+# Copying files
 [ -d "./niri" ] && cp -v ./niri/config.kdl ~/.config/niri/
 [ -d "./waybar" ] && cp -v ./waybar/*.{jsonc,css} ~/.config/waybar/
 [ -d "./alacritty" ] && cp -v ./alacritty/alacritty.toml ~/.config/alacritty/
@@ -16,7 +18,7 @@ else
     echo "Warning: ./wallpaper/wallpaper.jpg not found, skipping copy."
 fi
 
-# Write Portals Config
+# Portals Config
 cat <<EOF > ~/.config/xdg-desktop-portal/portals.conf
 [preferred]
 default=wlr;gtk
@@ -26,12 +28,6 @@ EOF
 
 # Fish Config
 cat <<EOF > ~/.config/fish/config.fish
-# if status is-login
-#     if test -z "\$DISPLAY" -a "\$XDG_VTNR" = 1
-#         exec niri-session
-#     end
-# end
-
 function rwb
     killall waybar
     waybar > /dev/null 2>&1 &
@@ -44,14 +40,3 @@ EOF
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 fisher install IlanCosman/tide@v6
-
-
-
-# 7. Set up Auto-login for Getty
-# echo "--- Setting up Auto-login ---"
-# sudo mkdir -p /etc/systemd/system/getty@tty1.service.d/
-# sudo bash -c "cat <<EOF > /etc/systemd/system/getty@tty1.service.d/autologin.conf
-# [Service]
-# ExecStart=
-# ExecStart=-/sbin/agetty --o '-p -- \\\\u' --noclear --autologin $USER %I \$TERM
-# EOF"
--- install.sh
+++ install.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-# Install Pacman Packages
+# Refresh package database
+sudo pacman -Sy
+
+# Install Pacman Packages (Added niri here and fixed swaylock typo)
 echo "--- Installing Pacman Packages ---"
-sudo pacman -Syu --needed --noconfirm \
-curl wget git xwayland-satellite xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-wlr \
+sudo pacman -S --needed --noconfirm \
+niri curl wget git xwayland-satellite xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-wlr \
 xdg-desktop-portal-gtk alacritty neofetch nano micro neovim fish btop polkit-gnome pacman-contrib timeshift \
-reflector network-manager-applet thunar gvfs tumbler vlc firefox swaybg ddcutil brightnessctl \
-wireplumber wl-clipboard cliphist fuzzel swaync swayloack cava wlsunset evince gedit glances udiskie qt6-multimedia-ffmpeg \
+reflector networkmanager network-manager-applet thunar gvfs tumbler vlc firefox swaybg ddcutil brightnessctl \
+wireplumber wl-clipboard cliphist fuzzel swaync swaylock cava wlsunset evince gedit glances udiskie qt6-multimedia-ffmpeg \
 ifuse usbmuxd libplist libimobiledevice inter-font ttf-jetbrains-mono-nerd tar bzip2 gzip unzip unrar \
 wireguard-tools localsend waybar playerctl libappindicator-gtk3
 
-# Install Yay (AUR Helper)
+# Install Yay
 echo "--- Installing Yay ---"
-sudo pacman -S --needed --noconfirm base-devel
-git clone https://aur.archlinux.org/yay.git
-cd yay && makepkg -si --noconfirm && cd .. && rm -rf yay
-
-# Install niri
-echo "--- Installing niri ---"
-yay -S --noconfirm niri
+if ! command -v yay &> /dev/null; then
+    sudo pacman -S --needed --noconfirm base-devel
+    git clone https://aur.archlinux.org/yay.git
+    cd yay && makepkg -si --noconfirm && cd .. && rm -rf yay
+fi
 
 # Install AUR Packages
 echo "--- Installing AUR Packages ---"
 yay -S --noconfirm waypaper wlogout nwg-look unimatrix-git visual-studio-code-bin
 
-# Install sddm (login manager)
-echo "--- Installing Display Manager (sddm) ---"
-sudo pacman -S sddm
-sudo systemctl enable sddm.service
-sudo systemctl start sddm.service
+# Install and Enable sddm
+echo "--- Setting up SDDM ---"
+sudo pacman -S --noconfirm sddm
+sudo systemctl enable sddm

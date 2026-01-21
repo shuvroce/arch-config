b0fd682fbd07f2f3568845331374541f24cb12e2
Md. Akram Hossain <akramshuvro@gmail.com>
2026-01-22 00:17:19 +0600

    update

--- scripts/ytdlp.sh
+++ scripts/ytdlp.sh
@@ -11,7 +11,7 @@ fi
 # Move to the Thunar directory
 cd "$1"
 
-echo "--- YouTube Downloader ---"
+echo "--- Universal Video Downloader ---"
 echo "URL: $URL"
 echo "Select desired quality:"
 
@@ -22,11 +22,15 @@ options=("720p (MP4)" "1080p (MP4)" "Best Available (Any)" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
-        "720p (MP4)")
+        "480p")
+            FORMAT="bv*[height<=480][ext=mp4]+ba[ext=m4a]/b[height<=480]"
+            break
+            ;;
+        "720p")
             FORMAT="bv*[height<=720][ext=mp4]+ba[ext=m4a]/b[height<=720]"
             break
             ;;
-        "1080p (MP4)")
+        "1080p")
             FORMAT="bv*[height<=1080][ext=mp4]+ba[ext=m4a]/b[height<=1080]"
             break
             ;;

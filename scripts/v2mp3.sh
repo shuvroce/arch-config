35d93c1d46207e8424f4e579addc6c62d934b0ba
Md. Akram Hossain <akramshuvro@gmail.com>
2026-01-23 11:46:21 +0600

    update

--- scripts/v2mp3.sh
+++ scripts/v2mp3.sh
@@ -5,6 +5,15 @@ for input_file in "$@"; do
     if [[ "$input_file" == *.mp3 ]]; then
         continue
     fi
-    ffmpeg -i "$input_file" -vn -acodec libmp3lame -q:a 2 "$output_file" -y -loglevel quiet
+    
+    input_name=$(basename "$input_file")
+    output_name=$(basename "$output_file")
+    
+    echo "Converting: $input_name ..."
+    
+    ffmpeg -i "$input_file" -vn -acodec libmp3lame -q:a 2 "$output_file" -y -loglevel error
+    
+    echo "Converted to: $output_name"
+    echo -e "--------------------------------------------------- \n"
 done
 notify-send "Conversion Complete" "Video files converted to MP3"

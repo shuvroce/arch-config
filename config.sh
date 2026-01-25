916ddc9537fec6dd46adfcb75cd18885d6b443a9
Md. Akram Hossain <akramshuvro@gmail.com>
2026-01-25 14:57:44 +0600

    update

--- config.sh
+++ config.sh
@@ -2,19 +2,25 @@
 
 # Create Directories
 echo "--- Setting up Configs ---"
-mkdir -p ~/.config/{xdg-desktop-portal,fish,niri,waybar,alacritty,fuzzel,Thunar}
+mkdir -p ~/.config/{xdg-desktop-portal,fish,niri,waybar,alacritty,fuzzel,Thunar,mako,qt5ct,qt6ct,wlogout}
 mkdir -p ~/.config/fish/functions
+mkdir -p ~/.config/qt5ct/colors
+mkdir -p ~/.config/qt6ct/colors
 mkdir -p ~/.local/bin
 mkdir -p ~/Pictures/Wallpaper
 
 # Copying files
-[ -d "./niri" ]      && cp -rv ./niri/config.kdl ~/.config/niri/
-[ -d "./waybar" ]    && cp -rv ./waybar/* ~/.config/waybar/
-[ -d "./alacritty" ] && cp -rv ./alacritty/alacritty.toml ~/.config/alacritty/
+[ -d "./niri" ]           && cp -rv ./niri/config.kdl ~/.config/niri/
+[ -d "./waybar" ]         && cp -rv ./waybar/* ~/.config/waybar/
+[ -d "./alacritty" ]      && cp -rv ./alacritty/alacritty.toml ~/.config/alacritty/
 [ -d "./fish/functions" ] && cp -rv ./fish/functions/* ~/.config/fish/functions/
-[ -d "./fuzzel" ]    && cp -rv ./fuzzel/fuzzel.ini ~/.config/fuzzel/
-[ -d "./Thunar" ]    && cp -rv ./Thunar/uca.xml ~/.config/Thunar/
-[ -d "./scripts" ]   && cp -rv ./scripts/* ~/.local/bin/
+[ -d "./fuzzel" ]         && cp -rv ./fuzzel/fuzzel.ini ~/.config/fuzzel/
+[ -d "./Thunar" ]         && cp -rv ./Thunar/uca.xml ~/.config/Thunar/
+[ -d "./mako" ]           && cp -rv ./mako/config ~/.config/mako/
+[ -d "./qt5ct/colors" ]   && cp -rv ./qt5ct/colors/* ~/.config/qt5ct/colors/
+[ -d "./qt6ct/colors" ]   && cp -rv ./qt6ct/colors/* ~/.config/qt6ct/colors/
+[ -d "./wlogout" ]        && cp -rv ./wlogout/* ~/.config/wlogout/
+[ -d "./scripts" ]        && cp -rv ./scripts/* ~/.local/bin/
 
 # Make all lcoal scripts executable
 echo "--- Making all lcoal scripts executable ---"
